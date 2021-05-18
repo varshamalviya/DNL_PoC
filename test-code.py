c03a00f47e44cf87e8b1ea7501bb4a2c4543de21
varshamalviya <45075712+varshamalviya@users.noreply.github.com>
2021-05-18 10:25:28 +0530

    delete file

--- test-code.py
+++
@@ -1,8 +0,0 @@
-str1 = input("Please Enter your String : ")
-ch = input("Please Enter your Character : ")
-newch = input("Please Enter the New Character : ")
-
-str2 = str1.replace(ch, newch)
-
-print("\nOriginal String :  ", str1)
-print("Modified String :  ", str2)
